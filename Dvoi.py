c4ab281da68be91ab332e2947bf0b4eb4345fae6
NoraxMiner <NoraxMiner@tutamail.com>
2024-10-14 15:10:11 +0300

    Dvoi

--- Dvoi.py
+++ Dvoi.py
@@ -1,12 +1,11 @@
 input_data = open('input.txt', 'r') 
 data= input_data.read() 
 #-------------------------------------------------------------------------
-n = int(data[0])
-d = bin(n)
-n =  d.count('1')
+n = bin(int(data))
+m =  n.count('1')
 #-------------------------------------------------------------------------
 output_data = open('output.txt', 'w') 
-output_data.write(str(n)) 
+output_data.write(str(m)) 
 
 input_data.close() 
 output_data.close()
